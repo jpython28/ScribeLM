4b35b4b6b9c8057b7ceca1a3b3d519e739e417bc
Jacob Schaller <jschaller2028@gmail.com>
2026-06-12 16:56:59 -0400

    Added troubleshooting

--- train.py
+++ train.py
@@ -43,7 +43,7 @@ if os.path.exists(wandb_config_path):
     wandb_entity = wandb_config["wandb"]["entity"]
     wandb_project = wandb_config["wandb"]["project"]
 else:
-    print("wandb config not found, not logging to wanb.")
+    print(f"wandb config not found at {wandb_config_path}, not logging to wanb.")
     use_wandb = False
     wandb_entity = None
     wandb_project = None
